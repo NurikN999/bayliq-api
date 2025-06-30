64a883191da0ab8f13179eba2c8e883935ffb715
NurikN999 <nurkamalnurmukhamed@gmail.com>
2025-06-30 20:30:01 +0500

    feat: add JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable, HasUuids;
 
@@ -26,5 +27,15 @@ class User extends Authenticatable
             'birth_date' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
